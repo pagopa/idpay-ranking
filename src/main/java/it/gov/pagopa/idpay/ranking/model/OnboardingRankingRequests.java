874d250115d98a7cdeb79279bf163a2e45bfdd98
GiovanaSolorzano <gisolorz@emeal.nttdata.com>
2022-11-22 17:25:47 +0100

    IREER-198 initializing ranking structure

---
+++ src/main/java/it/gov/pagopa/idpay/ranking/model/OnboardingRankingRequests.java
@@ -0,0 +1,28 @@
+package it.gov.pagopa.idpay.ranking.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode(of = {"id"}, callSuper = false)
+@Document(collection = "onboarding_ranking_requests")
+public class OnboardingRankingRequests {
+    @Id
+    private String id;
+    @NotEmpty
+    private String userId;
+    @NotEmpty
+    private String initiativeId;
+    @NotNull
+    private LocalDateTime admissibilityCheckDate;
+    private LocalDateTime criteriaConsensusTimestamp;
+    private long rankingValue;
+}
---
+++ src/main/java/it/gov/pagopa/idpay/ranking/service/OnboardingRankingRequestsService.java
@@ -0,0 +1,7 @@
+package it.gov.pagopa.idpay.ranking.service;
+
+import it.gov.pagopa.idpay.ranking.model.OnboardingRankingRequests;
+
+public interface OnboardingRankingRequestsService {
+    OnboardingRankingRequests save(OnboardingRankingRequests onboardingRankingRequests);
+}
