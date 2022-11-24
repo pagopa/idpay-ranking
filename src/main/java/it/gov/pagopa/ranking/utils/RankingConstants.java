a79912bf446bd35758da1766704be712937cb6b3
GiovanaSolorzano <gisolorz@emeal.nttdata.com>
2022-11-24 16:11:11 +0100

    IREER-204 Added dto

---
+++ src/main/java/it/gov/pagopa/ranking/utils/RankingConstants.java
@@ -0,0 +1,13 @@
+package it.gov.pagopa.ranking.utils;
+
+public final class RankingConstants {
+    private RankingConstants(){}
+
+    //region initiative ranking status
+    public static final String INITIATIVE_RANKING_STATUS_WAITING_END = "WAITING_END";
+    public static final String INITIATIVE_RANKING_STATUS_BUILDING = "BUILDING";
+    public static final String INITIATIVE_RANKING_STATUS_NOTIFY = "NOTIFY";
+    public static final String INITIATIVE_RANKING_STATUS_READY = "READY";
+    public static final String INITIATIVE_RANKING_STATUS_COMPLETED = "COMPLETED";
+    //endregion
+}
