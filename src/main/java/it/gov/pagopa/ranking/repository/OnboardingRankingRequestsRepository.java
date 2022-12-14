fbfb4f3cc39e0988e671e428c18555ed063d6bfa
anttorre <antonio.torre@nttdata.com>
2022-12-14 18:41:54 +0100

    merge release-dev into IREER-205-sviluppo-batch-finalizzazione-graduatoria

--- src/main/java/it/gov/pagopa/ranking/repository/OnboardingRankingRequestsRepository.java
+++ src/main/java/it/gov/pagopa/ranking/repository/OnboardingRankingRequestsRepository.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.ranking.repository;
 
 import it.gov.pagopa.ranking.model.OnboardingRankingRequests;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
