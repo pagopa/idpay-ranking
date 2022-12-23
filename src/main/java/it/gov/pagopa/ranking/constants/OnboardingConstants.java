55f4d1949ecfc02e58b3984b6fe48e0e0e139c0d
Stefano Terragni <stefano.terragni@everis.com>
2022-12-23 16:55:49 +0100

    Inserted rejectionReason "code" for ELIGIBLE_KO

---
+++ src/main/java/it/gov/pagopa/ranking/constants/OnboardingConstants.java
@@ -0,0 +1,14 @@
+package it.gov.pagopa.ranking.constants;
+
+public final class OnboardingConstants {
+    private OnboardingConstants(){}
+
+    //region onboarding statuses
+    public static final String ONBOARDING_STATUS_KO = "ONBOARDING_KO";
+    public static final String ONBOARDING_STATUS_OK = "ONBOARDING_OK";
+    //endregion
+
+    //region rejection reasons
+    public static final String REJECTION_REASON_CITIZEN_OUT_OF_RANKING = "CITIZEN_OUT_OF_RANKING";
+    //endregion
+}
